b61086059e06216b7ca6584cf2c0f362be1bec0a
ashaupperla-oc <105246054+ashaupperla-oc@users.noreply.github.com>
2022-07-31 14:25:18 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'development' ? '' : '/survey-frontend/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
